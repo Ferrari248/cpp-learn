d068254dbd1bf9f6319184c26bd7fb03c2ae9b13
maxliu <ly_chinese@163.com>
2024-11-26 23:40:46 +0800

    lk container-with-most-water

---
+++ lk/hot/double_ptr/container_with_most_water.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+/**
+ * https://leetcode.cn/problems/container-with-most-water/?envType=study-plan-v2&envId=top-100-liked
+ */
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int len = height.size();
+        int left = 0, right = len - 1;
+        int ans = 0;
+        while (left < right) {
+            int water_vol = min(height[left], height[right]) * (right - left);
+            ans = max(ans, water_vol);
+            if (height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> v1{1,8,6,2,5,4,8,3,7};
+    cout << solution.maxArea(v1) << endl;
+    vector<int> v2{1,1};
+    cout << solution.maxArea(v2) << endl;
+}

b1862dc99ab57d4e2bc73c16f2d332e3b5f6efc0
maxliu <ly_chinese@163.com>
2024-12-24 23:09:14 +0800

    string more usage

--- lk/prac/string_test.cpp
+++ lk/prac/string_test.cpp
@@ -37,6 +37,59 @@ void test_03() {
   cout << s1 << endl;
 }
 
+void test_04() {
+  string s1 = "abcdefg de";
+  int pos = s1.find("de");
+  cout << pos << endl;
+  int pos2 = s1.rfind("de");
+  cout << pos2 << endl;
+
+  string str1 = "abcdefgde";
+  str1.replace(1, 3, "11111111");
+  cout << "str1 = " << str1 << endl;
+}
+
+void test_05() {
+  string s1 = "abcdefg de";
+  string s2 = "abdcdefg de";
+  int comp_ret = s1.compare(s2);
+  cout << comp_ret << endl;
+  cout << (s1>s2) << endl;
+  cout << (s1==s2) << endl;
+  cout << (s1<s2) << endl;
+}
+
+void test_06() {
+  string s1 = "abcdefg de";
+  char c = s1[0];
+  char c2 = s1.at(1);
+  cout << c << endl;
+  cout << c2 << endl;
+  s1[3] = 'x';
+  s1.at(4) = 'y';
+  cout << s1 << endl;
+}
+
+void test_07() {
+  string s1 = "abcdefg de";
+  s1.insert(2, "c");
+  cout << s1 << endl;
+  s1.insert(2, 5, 'p');
+  cout << s1 << endl;
+  s1.erase(8);
+  cout << s1 << endl;
+  s1.erase(6, 1);
+  cout << s1 << endl;
+}
+
+void test_08() {
+  string s1 = "abcdefg de";
+  s1.substr(3, 5);
+  cout << s1 << endl;
+  string s2 = s1.substr(3, 9);
+  cout << s2 << endl;
+}
+
 int main() {
-  test_03();
+  test_08();
 }

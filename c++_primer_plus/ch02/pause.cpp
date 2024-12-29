ade1e11f1a66fff6de54891d431eb09b56cb1d96
maxliu <ly_chinese@163.com>
2024-12-29 20:55:58 +0800

    c4p again

---
+++ c++_primer_plus/ch02/pause.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  std::cout << "start" << endl;
+  cin.get();
+  cout << "end" << std::endl;
+  return 0;
+}
---
+++ c++_primer_plus/ch03/limist.cpp
@@ -0,0 +1,16 @@
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  short a1 = SHRT_MAX;
+  short a2 = SHRT_MIN;
+  cout << a1 << endl;
+  cout << a2 << endl;
+
+  int a3 = INT_MAX;
+  cout << a3 << endl;
+  int a4 = INT_MIN;
+  cout << a4 << endl;
+}

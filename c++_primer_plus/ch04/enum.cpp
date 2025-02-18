edbd29e1f09982b51e49bf1cd527c8c7599c28df
maxliu <ly_chinese@163.com>
2025-02-19 00:12:32 +0800

    c++11 enum

--- c++_primer_plus/ch04/enum.cpp
+++ c++_primer_plus/ch04/enum.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 using namespace std;
 
+// c风格老式枚举
 enum Color {
   RED,
   BLUE,
   GREEN,
 };
 
+// c++11强类型枚举
+enum class Season {
+  SPRING,
+  SUMMER,
+  AUTUMN,
+  WINTER,
+};
+
 int main() {
   enum Color c;
   c = BLUE;
   cout << c << endl;
-  int c2 = GREEN;
+  int c2 = Color::GREEN;
   cout << c2 << endl;
   c2 = Color(RED);
   c2 = Color::RED;
   cout << c2 << endl;
+
+  cout << "=====" << endl;
+
+  enum Season s = Season::SUMMER;
+  // int s1 = Season::SUMMER; err
+  // cout << s << endl; err
 
   return 0;
 }
